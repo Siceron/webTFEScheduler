1375c88ab56e3b620fe1bb010892deeca6c86d57
Siceron <ludovicfastre@yahoo.fr>
2017-02-06 11:54:37 +0100

    Avoid errors on input file

--- adminLTE.py
+++ adminLTE.py
@@ -1,6 +1,7 @@
 import web
 import csv
 import sys
+import os
 from models import *
 from json_utils import *
 from subprocess import Popen, PIPE, STDOUT
@@ -59,6 +60,8 @@ class index:
         x = web.input(myfile={})
         filedir = 'files' # change this to the directory you want to store the file in.
         if 'myfile' in x: # to check if the file-object is created
+            if x.myfile.filename == "":
+                return "Erreur : Pas de fichier"
             filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
             filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
             fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
@@ -76,6 +79,8 @@ class index:
             reader = csv.reader(f, delimiter=';')  # creates the reader object
             for row in reader:   # iterates the rows of the file in orders
                 if header:
+                    if (row[0] != "Code" or row[1] != "Titre" or row[2] != "Etudiants" or row[3] != "Promoteurs" or row[4] != "Lecteurs" or row[20] != "Mails"):
+                        return "Erreur : Mauvais fichier"
                     header = False
                 else:
                     if row[3] != "" and row[4] != "":
@@ -96,16 +101,16 @@ class index:
                             for prom in row[3].split(" - "):
                                 prom_name = prom.split(", ")
                                 if Advisor.select(Advisor.q.email == emails[email_count]).count() == 0:
-                                    if random_disp==True:
-                                        if Reader.select(Reader.q.email == emails[email_count]).count() == 0:
+                                    if Reader.select(Reader.q.email == emails[email_count]).count() == 0:
+                                        if random_disp==True:
                                             promoteur = Advisor(email=emails[email_count], last_name=prom_name[0],name=prom_name[1], disponibility=get_rand_disp())
-                                            tfe_rel_advisor = Tfe_rel_advisor(tfe=memoire, advisor=promoteur)
+                                            tfe_rel_advisor = Tfe_rel_advisor(tfe=memoire, advisor=promoteur)   
                                         else:
-                                            disp = Reader.select(Reader.q.email == emails[email_count])[0].disponibility
-                                            promoteur = Advisor(email=emails[email_count], last_name=prom_name[0],name=prom_name[1], disponibility=disp)
+                                            promoteur = Advisor(email=emails[email_count], last_name=prom_name[0],name=prom_name[1], disponibility=Disponibility())
                                             tfe_rel_advisor = Tfe_rel_advisor(tfe=memoire, advisor=promoteur)
                                     else:
-                                        promoteur = Advisor(email=emails[email_count], last_name=prom_name[0],name=prom_name[1], disponibility=Disponibility())
+                                        disp = Reader.select(Reader.q.email == emails[email_count])[0].disponibility
+                                        promoteur = Advisor(email=emails[email_count], last_name=prom_name[0],name=prom_name[1], disponibility=disp)
                                         tfe_rel_advisor = Tfe_rel_advisor(tfe=memoire, advisor=promoteur)
                                 else:
                                     tfe_rel_advisor = Tfe_rel_advisor(tfe=memoire, advisor=Advisor.select(Advisor.q.email == emails[email_count])[0])
@@ -116,31 +121,31 @@ class index:
                                     if Reader.select(Reader.q.email == emails[email_count]).count() == 0:
                                         if len(lect_name) == 2:
                                             lect_descr = lect_name[1].split(' ')
-                                            if random_disp==True:
-                                                if Advisor.select(Advisor.q.email == emails[email_count]).count() == 0:
+                                            if Advisor.select(Advisor.q.email == emails[email_count]).count() == 0:
+                                                if random_disp==True:
                                                     lecteur = Reader(email=emails[email_count], last_name=lect_name[0],name=lect_descr[0], disponibility=get_rand_disp())
                                                     tfe_rel_reader = Tfe_rel_reader(tfe=memoire, reader=lecteur)
                                                 else:
-                                                    disp = Advisor.select(Advisor.q.email == emails[email_count])[0].disponibility
-                                                    lecteur = Reader(email=emails[email_count], last_name=lect_name[0],name=lect_descr[0], disponibility=disp)
+                                                    lecteur = Reader(email=emails[email_count], last_name=lect_name[0],name=lect_descr[0], disponibility=Disponibility())
                                                     tfe_rel_reader = Tfe_rel_reader(tfe=memoire, reader=lecteur)
                                             else:
-                                                lecteur = Reader(email=emails[email_count], last_name=lect_name[0],name=lect_descr[0], disponibility=Disponibility())
+                                                disp = Advisor.select(Advisor.q.email == emails[email_count])[0].disponibility
+                                                lecteur = Reader(email=emails[email_count], last_name=lect_name[0],name=lect_descr[0], disponibility=disp)
                                                 tfe_rel_reader = Tfe_rel_reader(tfe=memoire, reader=lecteur)
                                             email_count += 1
                                         else:
                                             lect_name = lect.split(' ')
                                             if len(lect_name) == 4 or len(lect_name) == 3:
-                                                if random_disp==True:
-                                                    if Advisor.select(Advisor.q.email == emails[email_count]).count() == 0:
+                                                if Advisor.select(Advisor.q.email == emails[email_count]).count() == 0:
+                                                    if random_disp==True:
                                                         lecteur = Reader(email=emails[email_count], last_name=lect_name[2],name=lect_name[1], disponibility=get_rand_disp())
                                                         tfe_rel_reader = Tfe_rel_reader(tfe=memoire, reader=lecteur)
                                                     else:
-                                                        disp = Advisor.select(Advisor.q.email == emails[email_count])[0].disponibility
-                                                        lecteur = Reader(email=emails[email_count], last_name=lect_name[2],name=lect_name[1], disponibility=disp)
+                                                        lecteur = Reader(email=emails[email_count], last_name=lect_name[2],name=lect_name[1], disponibility=Disponibility())
                                                         tfe_rel_reader = Tfe_rel_reader(tfe=memoire, reader=lecteur)
                                                 else:
-                                                    lecteur = Reader(email=emails[email_count], last_name=lect_name[2],name=lect_name[1], disponibility=Disponibility())
+                                                    disp = Advisor.select(Advisor.q.email == emails[email_count])[0].disponibility
+                                                    lecteur = Reader(email=emails[email_count], last_name=lect_name[2],name=lect_name[1], disponibility=disp)
                                                     tfe_rel_reader = Tfe_rel_reader(tfe=memoire, reader=lecteur)
                                                 email_count += 1
                                             else:
@@ -167,8 +172,11 @@ class index:
                             print(row[20])"""
                     else:
                         print("discarded : "+row[0])
+        except:
+            return "Erreur : Mauvais fichier"
         finally:
             f.close()      # closing
+            os.remove(filedir +'/'+ filename)
         raise web.seeother('/')
 
 class executescheduler:
